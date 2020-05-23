f47ed7a533f73da0b4360d5736d7c2dc49827460
gAkira <guilherme.akira11@gmail.com>
2020-05-22 22:39:59 -0300

    Add graphs of N=160 and L=0.25 for all exercises of parte_1

--- p1.h
+++ p1.h
@@ -24,10 +24,10 @@
 // Define qual exercicio da primeira parte deseja computar
 //-----------------------------------------------------------------------------
 //# define EX_A_FAKE
-# define EX_A
+//# define EX_A
 //# define EX_B
 //# define EX_C_1
-//# define EX_C_2
+# define EX_C_2
 
 // Define o ponto da fonte puntual aplicada (valido em EX_C_x)
 //-----------------------------------------------------------------------------
